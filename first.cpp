269efdc5afa9a4758b6667215c4d3ea914e0e180
rakesh <rakesh@binarynote.com>
2018-04-27 09:15:53 +0530

    first cpp program

---
+++ first.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int Hello = 10;
+	cout<<"Hello World";
+	cout<<"\n";
+	cout<<"Hello : ";
+	cout<<Hello;
+	
+	cout<<"\n";
+	cout<<"Hello : "<<Hello;
+	cout<<"\n";
+	cout<<"Hello World"<<"\n"<<"Hello :"<<Hello <<"\n"<<"Hello :"<<Hello;
+	
+	cout<<"\n";
+	
+	cout<<"Hello World \n Hello : "<<Hello<<" \n Hello : "<<Hello;
+	
+	return 0;
+}
